23fa980b6c719aa5fc4a319c993f41fa0c7a8e5c
iamShreyaD <shreyavbdeshpande@gmail.com>
2025-07-25 00:41:16 -0400

    files updated

---
+++ 32. Stonks.py
@@ -0,0 +1,14 @@
+
+# price_at(x) that finds the price on a given day x.
+# max_price(a, b) that finds the maximum price from day a to day b.
+# min_price(a, b) that finds the minimum price from day a to day b.
+# The parameters of the days will be in the range of 1 to 20 (since that is the period for the stock we are analyzing).
+
+stock_prices = [34.68, 36.09, 34.94, 33.97, 34.68, 35.82, 43.41, 44.29, 44.65, 53.56, 49.85, 48.71, 48.71, 49.94, 48.53, 47.03, 46.59, 48.62, 44.21, 47.21]
+
+x = int(input("Which day's stock price do you want to find from the following stock prices list starting from 0? "))
+def price_at(x):
+    return 
+
+def max_price(a, b):
+    return max(stock_prices)
