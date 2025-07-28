f557fc342e2d313af40afdb2ad78c9791956bbdf
iamShreyaD <shreyavbdeshpande@gmail.com>
2025-07-27 20:25:47 -0400

    changes committed on 7/27/2025

---
+++ 25. Reading List.py
@@ -0,0 +1,6 @@
+# Write code below 💖
+books = ['Harry Potter', '1984', 'The Fault in Our Stars', 'The Mom Test', 'Life in Code']
+books.append('Panchiko')
+books.remove('The Fault in Our Stars')
+books.pop(1)
+print(books)
---
+++ Are We There Yet.py
@@ -0,0 +1,6 @@
+answer = input("Are we there yet? ")
+
+while answer != "yes":
+    answer = input("Are we there yet? ")
+
+print("Yayyy! We are here!")
