b55e7184e7549e7deaf20555886977c38cbb7559
iamShreyaD <shreyavbdeshpande@gmail.com>
2025-07-31 19:21:04 -0400

    files updated

--- 41. Countdown.py
+++ 41. Countdown.py
@@ -1,5 +1,9 @@
-import datetime, bday_messages
-bday_messages.path.append("C:\Users\shrey\OneDrive\Documents\Codedex\bday_messages.py")
+import datetime, sys
+
+# to add folder path not file
+sys.path.append(r"C:\Users\shrey\OneDrive\Documents\Codedex")
+
+import bday_messages
 
 
 today = datetime.date.today()
@@ -11,8 +15,10 @@ time_difference = today - next_birthday
 days_away = next_birthday - today
 
 if today == next_birthday:
-    print(messages)
+    # access random_messages as it is variable of bday_messages.py
+    print(bday_messages.random_message)
 
 else:
-    print('My next birthday is ' + str(days_away) + ' days away!')
+    # add .days to extract just the number of days
+    print('My next birthday is ' + str(days_away.days) + ' days away!')
 
